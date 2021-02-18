2d24e1684341eddbc58ac1a3cfa3564a9b32e112
Made Indra <indra.pramana@telkom.co.id>
2021-02-18 15:17:57 +0700

    send api content to websocket client

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
+
+var clients []*websocket.Conn
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -20,6 +23,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer conn.Close()
+	clients = append(clients, conn)
 
 	for {
 		msgType, msg, err := conn.ReadMessage()
@@ -38,7 +42,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func echo(w http.ResponseWriter, r *http.Request) {
+	req, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	for _, c := range clients {
+		c.WriteMessage(1, req)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(req)
+}
+
 func main() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/echo", echo)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
